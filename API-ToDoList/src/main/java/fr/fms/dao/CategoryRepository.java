b9ef6e0c156c9a7d61ed4875823dbc8a35c3accd
Saralune <sarahlef59@hotmail.fr>
2022-08-08 12:33:34 +0200

    transform spring boot app in api rest

--- API-ToDoList/src/main/java/fr/fms/dao/CategoryRepository.java
+++ API-ToDoList/src/main/java/fr/fms/dao/CategoryRepository.java
@@ -9,11 +9,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.fms.entities.Category;
 import fr.fms.entities.Users;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Stagiaires10P
  *
  */
+@Repository
 public interface CategoryRepository  extends JpaRepository<Category, Long> {
 	List<Category> findAllByUsers(Users users);
 }
